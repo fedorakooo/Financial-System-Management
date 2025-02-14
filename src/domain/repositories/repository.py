08accd82597b94c704fd93d4efed4e585ad51ba6
Alexander Fedorako <fedorakooo@gmail.com>
2025-02-14 13:48:10 +0300

    Add type annotations to AbstractRepository

--- src/domain/repositories/repository.py
+++ src/domain/repositories/repository.py
@@ -1,29 +1,32 @@
 from abc import ABC, abstractmethod
-from typing import List, Optional
+from typing import List, Optional, TypeVar
+from pydantic import BaseModel
+
+Item = TypeVar('Item', bound=BaseModel)
 
 
 class AbstractRepository(ABC):
     @abstractmethod
-    async def add_one(self, item) -> None:
+    async def add_one(self, item: Item) -> Item:
         """Adds one item to the repository."""
-        raise NotImplementedError
+        pass
 
     @abstractmethod
-    async def find_all(self) -> List:
+    async def find_all(self) -> List[Item]:
         """Returns all items from the repository."""
-        raise NotImplementedError
+        pass
 
     @abstractmethod
-    async def find_by_id(self, id: int) -> Optional:
+    async def find_by_id(self, id: int) -> Optional[Item]:
         """Finds an item by its id, or returns None if not found."""
-        raise NotImplementedError
+        pass
 
     @abstractmethod
-    async def update_one(self, id: int, item) -> None:
+    async def update_one(self, id: int, item: Item) -> Item:
         """Updates one item in the repository by its id."""
-        raise NotImplementedError
+        pass
 
     @abstractmethod
     async def delete_one(self, id: int) -> None:
         """Deletes one item from the repository by its id."""
-        raise NotImplementedError
+        pass
