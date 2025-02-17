f94fa913c4dcb24873996373cf41117315d4a28c
Alexander Fedorako <fedorakooo@gmail.com>
2025-02-17 22:28:32 +0300

    Refactor UnitOfWork to use asyncpg

--- src/infrastructure/uow.py
+++ src/infrastructure/uow.py
@@ -1,19 +1,32 @@
-from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Optional
+import asyncpg
 
-from src.domain.uow import AbstractUnitOfWork
+from src.domain.abstractions.uow import AbstractUnitOfWork
+from src.infrastructure.database import DATABASE_URL
 
 
 class UnitOfWork(AbstractUnitOfWork):
-    def __init__(self, session: AsyncSession):
-        self.session = session
-        self.repositories = {}
+    def __init__(self):
+        self.dsn = DATABASE_URL
+        self.connection: Optional[asyncpg.Connection] = None
+
+    async def __aenter__(self):
+        self.connection = await asyncpg.connect(self.dsn)
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        if self.connection:
+            await self.connection.close()
 
     async def commit(self):
-        try:
-            await self.session.commit()
-        except Exception as e:
-            await self.session.rollback()
-            raise e
+        if self.connection:
+            await self.connection.execute("COMMIT")
 
     async def rollback(self):
-        await self.session.rollback()
+        if self.connection:
+            await self.connection.execute("ROLLBACK")
+
+    @classmethod
+    def get_unit_of_work(cls) -> 'UnitOfWork':
+        """Factory method to return a new UnitOfWork instance."""
+        return cls()
