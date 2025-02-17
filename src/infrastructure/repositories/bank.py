d05eeb72e892a86fc76e55db13deeefc78be544d
Alexander Fedorako <fedorakooo@gmail.com>
2025-02-17 22:23:44 +0300

    Refactor BankRepository to use SQLRepository

--- src/infrastructure/repositories/bank.py
+++ src/infrastructure/repositories/bank.py
@@ -1,7 +1,22 @@
-from src.domain.schemas.bank import BankBase
-from src.infrastructure.repositories.sqlalchemy_repo import SQLAlchemyRepository
+from src.domain.schemas.bank import BankRead
+from src.infrastructure.database import DatabaseConnection
 from src.infrastructure.models.banks import BankORM
+from src.infrastructure.repositories.sql import SQLRepository
 
 
-class BankRepository(SQLAlchemyRepository[BankBase]):
-    model = BankORM
+class BankRepository(SQLRepository[BankORM]):
+    table_name = "banks"
+    model_class = BankRead
+
+    @classmethod
+    async def create_table(cls) -> None:
+        schema = """CREATE TABLE IF NOT EXISTS banks (
+                        id SERIAL PRIMARY KEY,
+                        name VARCHAR(150) NOT NULL,
+                        bic VARCHAR(32) NOT NULL UNIQUE,
+                        address VARCHAR(255) NOT NULL,
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+                );"""
+        async with DatabaseConnection() as conn:
+            await conn.execute(schema)
