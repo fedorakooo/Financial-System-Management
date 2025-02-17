cd42e2ccca574103f2a53c27f6961d3d259c19a4
Alexander Fedorako <fedorakooo@gmail.com>
2025-02-17 22:21:56 +0300

    Simplify BankService by removing redundant conversions

--- src/services/banks/bank.py
+++ src/services/banks/bank.py
@@ -1,8 +1,8 @@
 from typing import List, Optional
 
-from src.domain.repositories.repository import AbstractRepository
+from src.domain.abstractions.repository import AbstractRepository
 from src.domain.schemas.bank import BankCreate, BankRead, BankUpdate
-from src.domain.uow import AbstractUnitOfWork
+from src.domain.abstractions.uow import AbstractUnitOfWork
 
 
 class BankService:
@@ -13,23 +13,21 @@ class BankService:
     async def create_bank(self, bank_create: BankCreate) -> BankRead:
         async with self.uow as uow:
             new_bank = await self.repository.add_one(bank_create)
-        return BankRead(**new_bank.__dict__)
+        return new_bank
 
     async def get_all_banks(self) -> List[BankRead]:
         banks = await self.repository.find_all()
-        return [BankRead(**bank.__dict__) for bank in banks]
+        return banks
 
     async def get_bank_by_id(self, id: int) -> Optional[BankRead]:
-        bank = await self.repository.find_by_id(id)
-        if bank is None:
-            return None
-        return BankRead(**bank.__dict__)
+        bank = await self.repository.find_by_id(id=id)
+        return bank
 
     async def update_bank_by_id(self, id: int, bank_update: BankUpdate) -> BankRead:
         async with self.uow as uow:
             await self.repository.update_one(id, bank_update)
         updated_bank = await self.repository.find_by_id(id)
-        return BankRead(**updated_bank.__dict__)
+        return updated_bank
 
     async def delete_bank(self, id: int) -> None:
         async with self.uow as uow:
