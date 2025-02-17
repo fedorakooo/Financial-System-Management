d9ba1c8d025d39105068fb59b73821b29200f45f
Alexander Fedorako <fedorakooo@gmail.com>
2025-02-17 22:37:46 +0300

    Add database initialization on startup

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 
 from src.api.main import router
+from src.infrastructure.repositories.bank import BankRepository
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup_event():
+    await BankRepository.create_table()
 
 app.include_router(router)
