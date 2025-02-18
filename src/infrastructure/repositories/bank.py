ce33513d1cc465e7e3d40784c0e930c90e00133a
Alexander Fedorako <fedorakooo@gmail.com>
2025-02-18 16:35:10 +0300

    Add trigger to update updated_at field in banks table

--- src/infrastructure/repositories/bank.py
+++ src/infrastructure/repositories/bank.py
@@ -1,22 +1,40 @@
-from src.domain.schemas.bank import BankRead
+from src.domain.schemas.bank import BankRead, BankBase
 from src.infrastructure.database import DatabaseConnection
-from src.infrastructure.models.banks import BankORM
 from src.infrastructure.repositories.sql import SQLRepository
 
 
-class BankRepository(SQLRepository[BankORM]):
+class BankRepository(SQLRepository[BankBase]):
     table_name = "banks"
     model_class = BankRead
 
     @classmethod
     async def create_table(cls) -> None:
-        schema = """CREATE TABLE IF NOT EXISTS banks (
-                        id SERIAL PRIMARY KEY,
-                        name VARCHAR(150) NOT NULL,
-                        bic VARCHAR(32) NOT NULL UNIQUE,
-                        address VARCHAR(255) NOT NULL,
-                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-                        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
-                );"""
+        create_table = """
+            CREATE TABLE IF NOT EXISTS banks (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(150) NOT NULL,
+                bic VARCHAR(32) NOT NULL UNIQUE,
+                address VARCHAR(255) NOT NULL,
+                created_at TIMESTAMP DEFAULT now() NOT NULL,
+                updated_at TIMESTAMP DEFAULT now() NOT NULL
+            );
+        """
+
+        create_trigger = """
+            CREATE OR REPLACE FUNCTION update_updated_at_column()
+            RETURNS TRIGGER AS $$
+            BEGIN
+                NEW.updated_at = now();
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
+
+            CREATE TRIGGER trigger_update_updated_at
+            BEFORE UPDATE ON banks
+            FOR EACH ROW
+            EXECUTE FUNCTION update_updated_at_column();
+        """
+
         async with DatabaseConnection() as conn:
-            await conn.execute(schema)
+            await conn.execute(create_table)
+            await conn.execute(create_trigger)
