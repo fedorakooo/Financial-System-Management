0663417c22d03738363099d51c06706c528e877b
Alexander Fedorako <fedorakooo@gmail.com>
2025-02-18 16:47:55 +0300

    Add user table setup to startup event

--- src/main.py
+++ src/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 
 from src.api.main import router
 from src.infrastructure.repositories.bank import BankRepository
+from src.infrastructure.repositories.user import UserRepository
 
 app = FastAPI()
 
 @app.on_event("startup")
 async def startup_event():
+    await UserRepository.create_table()
     await BankRepository.create_table()
 
 app.include_router(router)
