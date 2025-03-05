f4b0973ca24bfb909ed2562222b359dd8412246d
Alexander Fedorako <fedorakooo@gmail.com>
2025-03-06 00:55:59 +0300

    Added Unit of Work implementation for database transactions

---
+++ backend/src/infrastructure/database/uow.py
@@ -0,0 +1,39 @@
+from typing import Optional
+import asyncpg
+
+from src.domain.abstractions.uow import AbstractUnitOfWork
+from src.infrastructure.database.database import DATABASE_URL
+
+
+class UnitOfWork(AbstractUnitOfWork):
+    def __init__(self):
+        self.dsn = DATABASE_URL
+        self.transaction: Optional[asyncpg.transaction.Transaction] = None
+
+    async def __aenter__(self):
+        self.connection = await asyncpg.connect(self.dsn)
+        self.transaction = self.connection.transaction()
+        await self.transaction.start()
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        if exc:
+            await self.rollback()
+        else:
+            await self.commit()
+
+        if self.connection:
+            await self.connection.close()
+
+    async def commit(self):
+        if self.transaction:
+            await self.transaction.commit()
+
+    async def rollback(self):
+        if self.transaction:
+            await self.transaction.rollback()
+
+    @classmethod
+    def get_unit_of_work(cls) -> 'UnitOfWork':
+        """Factory method to return a new UnitOfWork instance."""
+        return cls()
