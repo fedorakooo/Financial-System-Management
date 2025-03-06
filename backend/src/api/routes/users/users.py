c7a3bbe33cc5f192605b39a4045a5c4103863522
Alexander Fedorako <fedorakooo@gmail.com>
2025-03-06 23:20:32 +0300

    Added log entries for user actions

--- backend/src/api/routes/users/users.py
+++ backend/src/api/routes/users/users.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from typing import List
 
+from src.dependencies.logs import LogDependencies
 from src.dependencies.user import UserDependencies
 from src.domain.exceptions.repository import NotFoundError, NoFieldsToUpdateError, UniqueConstraintError
 from src.domain.schemas.user import UserRead, UserUpdate
+from src.services.logs.log import LogService
 from src.services.users.user import UserService
 
 router = APIRouter(prefix="/users", tags=["Users"])
@@ -15,16 +17,20 @@ router = APIRouter(prefix="/users", tags=["Users"])
 })
 async def get_user(
         user_id: int,
-        user_service: UserService = Depends(UserDependencies.get_user_service)
+        user_service: UserService = Depends(UserDependencies.get_user_service),
+        log_service: LogService = Depends(LogDependencies.get_log_service)
 ) -> UserRead:
     try:
         user = await user_service.get_user_by_id(user_id)
+        log_service.info(f"Successfully fetched user with ID {user_id}")
     except NotFoundError as e:
+        log_service.warning(f"User with ID {user_id} not found: {str(e)}")
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=str(e)
         )
     except Exception as e:
+        log_service.error(f"Unexpected error fetching user with ID {user_id}: {str(e)}")
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"An unexpected error occurred fetching the user."
@@ -36,11 +42,14 @@ async def get_user(
     500: {"description": "Unexpected server error"}
 })
 async def get_users(
-        user_service: UserService = Depends(UserDependencies.get_user_service)
+        user_service: UserService = Depends(UserDependencies.get_user_service),
+        log_service: LogService = Depends(LogDependencies.get_log_service)
 ) -> List[UserRead]:
     try:
         users = await user_service.get_all_users()
+        log_service.info(f"Successfully fetched {len(users)} users")
     except Exception as e:
+        log_service.error(f"Unexpected error fetching users: {str(e)}")
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"An unexpected error occurred fetching fetching the list of users."
@@ -57,29 +66,33 @@ async def get_users(
 async def update_user_by_id(
         user_id: int,
         user_update: UserUpdate,
-        user_service: UserService = Depends(UserDependencies.get_user_service)
+        user_service: UserService = Depends(UserDependencies.get_user_service),
+        log_service: LogService = Depends(LogDependencies.get_log_service)
 ) -> UserRead:
+    log_service.info(f"Updating user with ID {user_id}")
     try:
         updated_user = await user_service.update_user_by_id(user_id, user_update)
+        log_service.info(f"User with ID {user_id} successfully updated")
     except NotFoundError as e:
-        error_message = str(e)
+        log_service.error(f"User with ID {user_id} not found for update: {str(e)}")
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=error_message
+            detail=str(e)
         )
     except NoFieldsToUpdateError as e:
-        error_message = str(e)
+        log_service.error(f"No fields to update for user with ID {user_id}: {str(e)}")
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=error_message
+            detail=str(e)
         )
     except UniqueConstraintError as e:
-        error_message = str(e)
+        log_service.error(f"Unique constraint violation while updating user with ID {user_id}: {str(e)}")
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
-            detail=error_message
+            detail=str(e)
         )
     except Exception as e:
+        log_service.error(f"Unexpected error while updating user with ID {user_id}: {str(e)}")
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"An unexpected error occurred while updating the bank with id {user_id}."
@@ -93,17 +106,21 @@ async def update_user_by_id(
 })
 async def delete_user_by_id(
         user_id: int,
-        user_service: UserService = Depends(UserDependencies.get_user_service)
+        user_service: UserService = Depends(UserDependencies.get_user_service),
+        log_service: LogService = Depends(LogDependencies.get_log_service)
 ):
+    log_service.info(f"Updating user with ID {user_id}")
     try:
         await user_service.delete_user_by_id(user_id)
+        log_service.info(f"User with ID {user_id} deleted successfully")
     except NotFoundError as e:
-        error_message = str(e)
+        log_service.warning(f"User with ID {user_id} not found for deletion: {str(e)}")
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=error_message
+            detail=str(e)
         )
     except Exception as e:
+        log_service.error(f"Unexpected error while deleting user with ID {user_id}: {str(e)}")
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"An unexpected error occurred while deleting the user."
--- backend/src/dependencies/auth.py
+++ backend/src/dependencies/auth.py
@@ -3,11 +3,13 @@ from typing import Optional
 from fastapi import Depends, HTTPException, status
 
 from src.api.security import oauth2_scheme
+from src.dependencies.logs import LogDependencies
 from src.dependencies.user import UserDependencies
 from src.domain.exceptions.repository import NotFoundError
 from src.domain.schemas.user import UserRead
 from src.services.auth.auth import AuthService
 from src.services.auth.jwt import JWTService
+from src.services.logs.log import LogService
 from src.services.users.user import UserService
 from src.domain.exceptions.jwt import ExpiredTokenError, InvalidTokenError, TokenDecodeError
 
@@ -20,30 +22,29 @@ class JWTDependencies:
     @staticmethod
     async def get_current_token_payload(
             token: str = Depends(oauth2_scheme),
-            jwt_service: JWTService = Depends(get_jwt_service)
+            jwt_service: JWTService = Depends(get_jwt_service),
+            log_service: LogService = Depends(LogDependencies.get_log_service)
     ) -> dict:
         try:
             payload = jwt_service.decode(token=token)
 
         except ExpiredTokenError as e:
-            error_message = str(e)
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
-                detail=error_message
+                detail=str(e)
             )
         except InvalidTokenError as e:
-            error_message = str(e)
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
-                detail=error_message
+                detail=str(e)
             )
         except TokenDecodeError as e:
-            error_message = str(e)
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
-                detail=error_message
+                detail=str(e)
             )
         except Exception as e:
+            log_service.error(f"Unexpected error during token processing: {str(e)}")
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail="An unexpected error occurred"
@@ -56,14 +57,14 @@ class AuthDependencies:
     @staticmethod
     def get_auth_service(
             user_service: UserService = Depends(UserDependencies.get_user_service),
-            jwt_service: JWTService = Depends(JWTDependencies.get_jwt_service)
+            jwt_service: JWTService = Depends(JWTDependencies.get_jwt_service),
     ) -> AuthService:
         return AuthService(user_service, jwt_service)
 
     @staticmethod
     async def get_current_auth_user(
             payload: dict = Depends(JWTDependencies.get_current_token_payload),
-            user_service: UserService = Depends(UserDependencies.get_user_service)
+            user_service: UserService = Depends(UserDependencies.get_user_service),
     ) -> UserRead:
         phone_number: Optional[str] = payload.get("sub")
 
@@ -75,7 +76,7 @@ class AuthDependencies:
 
         try:
             user = await user_service.get_user_by_phone_number(phone_number)
-        except NotFoundError:
+        except NotFoundError as e:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Token is invalid: user not found"
@@ -85,7 +86,7 @@ class AuthDependencies:
 
     @staticmethod
     async def get_current_active_auth_user(
-            user: UserRead = Depends(get_current_auth_user)
+            user: UserRead = Depends(get_current_auth_user),
     ) -> UserRead:
         user = await user
 
