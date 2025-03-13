358c9699fa2f13e57f3478d4dd47564dfcf445c1
Alexander Fedorako <fedorakooo@gmail.com>
2025-03-13 09:30:42 +0300

    Refactored Logger class to use dynamic configuration from logger_config

--- backend/src/infrastructure/logger/logger.py
+++ backend/src/infrastructure/logger/logger.py
@@ -1,22 +1,27 @@
 import logging
-
 from src.domain.abstractions.logger.logger import AbstractLogger
 
 
 class Logger(AbstractLogger):
-    def __init__(self, log_file: str = "app.log"):
+    def __init__(self, logger_config: dict):
+        format_str = logger_config['formatters']['formatter']['format']
+        log_level = logger_config['root']['level']
+
+        formatter = logging.Formatter(format_str)
+
         self.logger = logging.getLogger("application_log")
-        self.logger.setLevel(logging.INFO)
+        self.logger.setLevel(log_level)
 
-        if not self.logger.handlers:
-            console_handler = logging.StreamHandler()
-            formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-            console_handler.setFormatter(formatter)
-            self.logger.addHandler(console_handler)
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logger_config['handlers']['console']['level'])
+        console_handler.setFormatter(formatter)
 
-            file_handler = logging.FileHandler(log_file)
-            file_handler.setFormatter(formatter)
-            self.logger.addHandler(file_handler)
+        file_handler = logging.FileHandler(logger_config['handlers']['file']['filename'])
+        file_handler.setLevel(logger_config['handlers']['file']['level'])
+        file_handler.setFormatter(formatter)
+
+        self.logger.addHandler(console_handler)
+        self.logger.addHandler(file_handler)
 
     def info(self, message: str):
         self.logger.info(message)
