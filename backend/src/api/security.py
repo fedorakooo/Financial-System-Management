fe97180ad670ba9e2303a5e5c06f52f99862c88a
Alexander Fedorako <fedorakooo@gmail.com>
2025-03-18 01:32:38 +0300

    Added dependency injection for current active auth user

--- backend/src/api/security.py
+++ backend/src/api/security.py
@@ -1,3 +1,18 @@
 from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends
+from dependency_injector.wiring import Provide, inject
+
+from src.application.abstractions.auth.auth import AbstractAuthService
+from src.infrastructure.dependencies.app import Application
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+@inject
+def get_current_active_auth_user(
+        token: str = Depends(oauth2_scheme),
+        auth_service: AbstractAuthService = Depends(
+            Provide[Application.services.auth_service]
+        )
+):
+    return auth_service.get_current_active_auth_user(token)
