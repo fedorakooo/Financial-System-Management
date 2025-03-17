765f089353c075ee3021fab1db3f47a672a6b38e
Alexander Fedorako <fedorakooo@gmail.com>
2025-03-18 01:33:59 +0300

    Updated app initialization with CORS, dependency container, and startup logic

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from src.api.main import router
-from src.infrastructure.database.database_initializer import DatabaseInitializer
+from src.api.startup import app_startup
+from src.infrastructure.dependencies.setup import setup_container
+
+container = setup_container()
 
 app = FastAPI()
 
-app.add_event_handler("startup", DatabaseInitializer.create_all_tables)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.container = container
+
+app.add_event_handler("startup", app_startup)
 
 app.include_router(router)
