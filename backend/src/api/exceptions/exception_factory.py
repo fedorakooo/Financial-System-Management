ae25e1183d3c73113b58d683a623416ad9836060
Alexander Fedorako <fedorakooo@gmail.com>
2025-03-18 01:29:15 +0300

    Added HttpExceptionFactory for creating HTTP exceptions

---
+++ backend/src/api/exceptions/exception_factory.py
@@ -0,0 +1,13 @@
+from fastapi import HTTPException
+
+
+class HttpExceptionFactory:
+    @staticmethod
+    def create_http_exception(
+            status_code: int,
+            detail: str
+    ) -> HTTPException:
+        raise HTTPException(
+            status_code=status_code,
+            detail=detail
+        )
