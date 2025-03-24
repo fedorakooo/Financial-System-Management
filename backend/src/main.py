e44597b1482a71db1f400e036dc9e949f6c73e62
Alexander Fedorako <fedorakooo@gmail.com>
2025-03-24 22:18:45 +0300

    Add CORS middleware to allow cross-origin requests

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from src.api.main import router
 
 from src.api.startup import app_startup
@@ -9,6 +11,14 @@ container = setup_container()
 app = FastAPI()
 
 app.container = container
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.add_event_handler("startup", app_startup)
 
